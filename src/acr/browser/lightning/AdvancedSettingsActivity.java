cc718d75281571f5402f9abdb7f80733c32b106a
Gastón Marrero <gaston@kidbox.net>
2014-10-24 18:29:14 -0200

    Se eliminaron varios avanzados

--- src/acr/browser/lightning/AdvancedSettingsActivity.java
+++ src/acr/browser/lightning/AdvancedSettingsActivity.java
@@ -22,7 +22,6 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 public class AdvancedSettingsActivity extends Activity {
 
@@ -30,8 +29,7 @@ public class AdvancedSettingsActivity extends Activity {
 	private static final int API = android.os.Build.VERSION.SDK_INT;
 	private SharedPreferences mPreferences;
 	private SharedPreferences.Editor mEditPrefs;
-	private CheckBox cb1, cb2, cb3, cb4, cb5, cb6, cb7, cb8, cb9, cb10, cb11, cbIncognitoCookies,
-			cbSearchSuggestions, cbClearHistoryExit, cbClearCookiesExit;
+	private CheckBox cb1, cb6, cb7, cbSearchSuggestions;
 	private Context mContext;
 	private boolean mSystemBrowser;
 	private Handler messageHandler;
@@ -70,135 +68,41 @@ public class AdvancedSettingsActivity extends Activity {
 
 	private void initialize() {
 
-		RelativeLayout r1, r2, r3, r4, r5, r6, r7, r8, r10, r11, r12, r13, r14, r15, rIncognitoCookies, rClearCache, rSearchSuggestions, rClearHistoryExit, rClearCookiesExit;
+		RelativeLayout r1, r6, r7, r8, r10, r15, rClearCache, rSearchSuggestions;
 
 		r1 = (RelativeLayout) findViewById(R.id.r1);
-		r2 = (RelativeLayout) findViewById(R.id.r2);
-		rClearHistoryExit = (RelativeLayout) findViewById(R.id.rClearHistoryExit);
-		rClearCookiesExit = (RelativeLayout) findViewById(R.id.rClearCookiesExit);
-		r3 = (RelativeLayout) findViewById(R.id.r3);
-		r4 = (RelativeLayout) findViewById(R.id.r4);
-		r5 = (RelativeLayout) findViewById(R.id.r5);
 		r6 = (RelativeLayout) findViewById(R.id.r6);
 		r7 = (RelativeLayout) findViewById(R.id.r7);
 		r8 = (RelativeLayout) findViewById(R.id.rClearHistory);
 		r10 = (RelativeLayout) findViewById(R.id.r10);
-		r11 = (RelativeLayout) findViewById(R.id.r11);
-		r12 = (RelativeLayout) findViewById(R.id.r12);
-		r13 = (RelativeLayout) findViewById(R.id.r13);
-		r14 = (RelativeLayout) findViewById(R.id.r14);
 		r15 = (RelativeLayout) findViewById(R.id.r15);
-		rIncognitoCookies = (RelativeLayout) findViewById(R.id.rIncognitoCookies);
 		rClearCache = (RelativeLayout) findViewById(R.id.rClearCache);
 		rSearchSuggestions = (RelativeLayout) findViewById(R.id.rGoogleSuggestions);
 
 		cb1 = (CheckBox) findViewById(R.id.cb1);
-		cb2 = (CheckBox) findViewById(R.id.cb2);
-		cbClearHistoryExit = (CheckBox) findViewById(R.id.cbClearHistoryExit);
-		cbClearCookiesExit = (CheckBox) findViewById(R.id.cbClearCookiesExit);
-		cb3 = (CheckBox) findViewById(R.id.cb3);
-		cb4 = (CheckBox) findViewById(R.id.cb4);
-		cb5 = (CheckBox) findViewById(R.id.cb5);
 		cb6 = (CheckBox) findViewById(R.id.cb6);
 		cb7 = (CheckBox) findViewById(R.id.cb7);
-		cb8 = (CheckBox) findViewById(R.id.cb8);
-		cb9 = (CheckBox) findViewById(R.id.cb9);
-		cb10 = (CheckBox) findViewById(R.id.cb10);
-		cb11 = (CheckBox) findViewById(R.id.cb11);
-		cbIncognitoCookies = (CheckBox) findViewById(R.id.cbIncognitoCookies);
 		cbSearchSuggestions = (CheckBox) findViewById(R.id.cbGoogleSuggestions);
 
 		cb1.setChecked(mPreferences.getBoolean(PreferenceConstants.SAVE_PASSWORDS, true));
-		cb2.setChecked(mPreferences.getBoolean(PreferenceConstants.CLEAR_CACHE_EXIT, false));
-		cbClearHistoryExit.setChecked(mPreferences.getBoolean(
-				PreferenceConstants.CLEAR_HISTORY_EXIT, false));
-		cbClearCookiesExit.setChecked(mPreferences.getBoolean(
-				PreferenceConstants.CLEAR_COOKIES_EXIT, false));
-		cb3.setChecked(mPreferences.getBoolean(PreferenceConstants.JAVASCRIPT, true));
-		cb4.setChecked(mPreferences.getBoolean(PreferenceConstants.TEXT_REFLOW, false));
-		cb4.setEnabled(API < 19);
-		if (API >= 19) {
-			mEditPrefs.putBoolean(PreferenceConstants.TEXT_REFLOW, false);
-			mEditPrefs.commit();
-		}
-		cb5.setChecked(mPreferences.getBoolean(PreferenceConstants.BLOCK_IMAGES, false));
+
 		cb6.setChecked(mPreferences.getBoolean(PreferenceConstants.POPUPS, true));
 		cb7.setChecked(mPreferences.getBoolean(PreferenceConstants.COOKIES, true));
-		cb8.setChecked(mPreferences.getBoolean(PreferenceConstants.USE_WIDE_VIEWPORT, true));
-		cb9.setChecked(mPreferences.getBoolean(PreferenceConstants.OVERVIEW_MODE, true));
-		cb10.setChecked(mPreferences.getBoolean(PreferenceConstants.RESTORE_LOST_TABS, true));
-		cb11.setChecked(mPreferences.getBoolean(PreferenceConstants.HIDE_STATUS_BAR, false));
-		cbIncognitoCookies.setChecked(mPreferences.getBoolean(
-				PreferenceConstants.INCOGNITO_COOKIES, false));
 		cbSearchSuggestions.setChecked(mPreferences.getBoolean(
 				PreferenceConstants.GOOGLE_SEARCH_SUGGESTIONS, true));
 
 		r1(r1);
-		r2(r2);
-		rClearHistoryExit(rClearHistoryExit);
-		rClearCookiesExit(rClearCookiesExit);
-		r3(r3);
-		r4(r4);
-		r5(r5);
 		r6(r6);
 		r7(r7);
 		r8(r8);
 		r10(r10);
-		r11(r11);
-		r12(r12);
-		r13(r13);
-		r14(r14);
 		r15(r15);
-		rIncognitoCookies(rIncognitoCookies);
 		rClearCache(rClearCache);
 		rSearchSuggestions(rSearchSuggestions);
 		cb1(cb1);
-		cb2(cb2);
-		cbClearHistoryExit(cbClearHistoryExit);
-		cbClearCookiesExit(cbClearCookiesExit);
-		cb3(cb3);
-		cb4(cb4);
-		cb5(cb5);
 		cb6(cb6);
 		cb7(cb7);
-		cb8(cb8);
-		cb9(cb9);
-		cb10(cb10);
-		cb11(cb11);
-		cbIncognitoCookies(cbIncognitoCookies);
 		cbSearchSuggestions(cbSearchSuggestions);
-
-		TextView syncHistory = (TextView) findViewById(R.id.isBrowserAvailable);
-
-		RelativeLayout layoutSyncHistory = (RelativeLayout) findViewById(R.id.rBrowserHistory);
-		final CheckBox cbSyncHistory = (CheckBox) findViewById(R.id.cbBrowserHistory);
-		layoutSyncHistory.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				cbSyncHistory.setChecked(!cbSyncHistory.isChecked());
-			}
-
-		});
-		cbSyncHistory.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				mEditPrefs.putBoolean(PreferenceConstants.SYNC_HISTORY, isChecked).apply();
-			}
-
-		});
-
-		if (!mSystemBrowser) {
-			cbSyncHistory.setChecked(false);
-			cbSyncHistory.setEnabled(false);
-			syncHistory.setText(getResources().getString(R.string.stock_browser_unavailable));
-		} else {
-			cbSyncHistory.setEnabled(true);
-			cbSyncHistory.setChecked(mPreferences
-					.getBoolean(PreferenceConstants.SYNC_HISTORY, true));
-			syncHistory.setText(getResources().getString(R.string.stock_browser_available));
-		}
 
 		messageHandler = new MessageHandler(mContext);
 	}
@@ -242,77 +146,6 @@ public class AdvancedSettingsActivity extends Activity {
 		});
 	}
 
-	private void cb2(CheckBox view) {
-		view.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				mEditPrefs.putBoolean(PreferenceConstants.CLEAR_CACHE_EXIT, isChecked);
-				mEditPrefs.commit();
-			}
-
-		});
-	}
-
-	private void cbClearHistoryExit(CheckBox view) {
-		view.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				mEditPrefs.putBoolean(PreferenceConstants.CLEAR_HISTORY_EXIT, isChecked);
-				mEditPrefs.commit();
-			}
-
-		});
-	}
-
-	private void cbClearCookiesExit(CheckBox view) {
-		view.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				mEditPrefs.putBoolean(PreferenceConstants.CLEAR_COOKIES_EXIT, isChecked);
-				mEditPrefs.commit();
-			}
-
-		});
-	}
-
-	private void cb3(CheckBox view) {
-		view.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				mEditPrefs.putBoolean(PreferenceConstants.JAVASCRIPT, isChecked);
-				mEditPrefs.commit();
-			}
-
-		});
-	}
-
-	private void cb4(CheckBox view) {
-		view.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				mEditPrefs.putBoolean(PreferenceConstants.TEXT_REFLOW, isChecked);
-				mEditPrefs.commit();
-			}
-		});
-	}
-
-	private void cb5(CheckBox view) {
-		view.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				mEditPrefs.putBoolean(PreferenceConstants.BLOCK_IMAGES, isChecked);
-				mEditPrefs.commit();
-			}
-
-		});
-	}
-
 	private void cb6(CheckBox view) {
 		view.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
@@ -337,65 +170,8 @@ public class AdvancedSettingsActivity extends Activity {
 		});
 	}
 
-	private void cb8(CheckBox view) {
-		view.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				mEditPrefs.putBoolean(PreferenceConstants.USE_WIDE_VIEWPORT, isChecked);
-				mEditPrefs.commit();
-			}
 
-		});
-	}
-
-	private void cb9(CheckBox view) {
-		view.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				mEditPrefs.putBoolean(PreferenceConstants.OVERVIEW_MODE, isChecked);
-				mEditPrefs.commit();
-			}
-
-		});
-	}
-
-	private void cb10(CheckBox view) {
-		view.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				mEditPrefs.putBoolean(PreferenceConstants.RESTORE_LOST_TABS, isChecked);
-				mEditPrefs.commit();
-			}
-
-		});
-	}
-
-	private void cb11(CheckBox view) {
-		view.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				mEditPrefs.putBoolean(PreferenceConstants.HIDE_STATUS_BAR, isChecked);
-				mEditPrefs.commit();
-			}
-
-		});
-	}
-
-	private void cbIncognitoCookies(CheckBox view) {
-		view.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				mEditPrefs.putBoolean(PreferenceConstants.INCOGNITO_COOKIES, isChecked);
-				mEditPrefs.commit();
-			}
-
-		});
-	}
 
 	private void cbSearchSuggestions(CheckBox view) {
 		view.setOnCheckedChangeListener(new OnCheckedChangeListener() {
@@ -416,84 +192,6 @@ public class AdvancedSettingsActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				cb1.setChecked(!cb1.isChecked());
-			}
-
-		});
-	}
-
-	private void r2(RelativeLayout view) {
-		view.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				cb2.setChecked(!cb2.isChecked());
-			}
-
-		});
-	}
-
-	private void rClearHistoryExit(RelativeLayout view) {
-		view.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				cbClearHistoryExit.setChecked(!cbClearHistoryExit.isChecked());
-			}
-
-		});
-	}
-
-	private void rClearCookiesExit(RelativeLayout view) {
-		view.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				cbClearCookiesExit.setChecked(!cbClearCookiesExit.isChecked());
-			}
-
-		});
-	}
-
-	private void r3(RelativeLayout view) {
-		view.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				cb3.setChecked(!cb3.isChecked());
-			}
-
-		});
-	}
-
-	private void r4(RelativeLayout view) {
-		view.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				if (API < 19) {
-					cb4.setChecked(!cb4.isChecked());
-				} else {
-					Utils.createInformativeDialog(mContext,
-							getResources().getString(R.string.title_warning), getResources()
-									.getString(R.string.dialog_reflow_warning));
-				}
-			}
-
-		});
-	}
-
-	private void r5(RelativeLayout view) {
-		view.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				cb5.setChecked(!cb5.isChecked());
 			}
 
 		});
@@ -563,52 +261,6 @@ public class AdvancedSettingsActivity extends Activity {
 		});
 	}
 
-	private void r11(RelativeLayout view) {
-		view.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				cb8.setChecked(!cb8.isChecked());
-			}
-
-		});
-
-	}
-
-	private void r12(RelativeLayout view) {
-		view.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				cb9.setChecked(!cb9.isChecked());
-			}
-
-		});
-	}
-
-	private void r13(RelativeLayout view) {
-		view.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				cb10.setChecked(!cb10.isChecked());
-			}
-
-		});
-	}
-
-	private void r14(RelativeLayout view) {
-		view.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				cb11.setChecked(!cb11.isChecked());
-			}
-
-		});
-	}
 
 	private void r15(RelativeLayout view) {
 		view.setOnClickListener(new OnClickListener() {
@@ -647,19 +299,6 @@ public class AdvancedSettingsActivity extends Activity {
 			}
 
 		});
-	}
-
-	private void rIncognitoCookies(RelativeLayout view) {
-		view.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				cbIncognitoCookies.setChecked(!cbIncognitoCookies.isChecked());
-			}
-
-		});
-
 	}
 
 	private void rSearchSuggestions(RelativeLayout view) {
